38167513eb6286f715647bdb310be926dadd2db6
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-05-26 16:36:45 +0530

    Implemented Code : Base Case Handling Left

--- HCS.py
+++ HCS.py
@@ -5,10 +5,10 @@
 
 
 """ Highly Connected Subgraph Algorithm : 
-      1. Find minimum cut 
-      2. Obtain Sub-graphs
-      3. Check which one of the subgraphs are not highly Connected
-      4. Return to Step 1 for those subgraphs                                                 """
+	  1. Find minimum cut 
+	  2. Obtain Sub-graphs
+	  3. Check which one of the subgraphs are not highly Connected
+	  4. Return to Step 1 for those subgraphs                                                 """
 
 
 #Libraries 
@@ -71,8 +71,114 @@ def self_loop_presence(g):
 			#print "Found"
 			
 
-    #Return Graph Data Structure without self-loops
+	#Return Graph Data Structure without self-loops
 	return g
+
+
+clusters = []
+
+#Function for Highly Connected Sub-graph Formation
+def HCS(connected_graph):
+	#Initialization for connected sub-graphs
+	temp_connected_graph = nx.Graph()
+	temp_connected_graph = connected_graph
+
+	#Find the Minimum Cut-edges
+	cut_edges = find_minimum_cut(temp_connected_graph)
+
+	total =  len(temp_connected_graph.edges())
+
+	number_of_nodes = len(temp_connected_graph.nodes())
+
+	edge_connectivity = len(cut_edges)
+
+	""" For a Graph to be HCS -- edge connectivity > number_of_nodes/2  """
+
+	#Condition True : HCS is valid
+	if edge_connectivity > (number_of_nodes/2): 
+		#Store the nodes in a temporary list
+		temp_list = []
+		temp_list.append(temp_connected_graph.nodes)
+		clusters.append(temp_list)
+
+    
+    #Divide the two components and pass into HCS
+	else:
+		#Removal of the edges from the Graph to form separate connected components
+		for item in cut_edges:
+			temp_connected_graph.remove_edge(item[0],item[1])
+
+
+		connected_components = list(nx.connected_components(temp_connected_graph))
+
+		#At this stage we will get Two Connected Components -- Compute & Store them separately
+		first_graph = nx.Graph()
+		second_graph = nx.Graph()
+
+		first_graph = temp_connected_graph.copy()
+		second_graph = temp_connected_graph.copy()
+
+		""" Separation of components """
+		#First Component
+		first_component = connected_components[0]
+		#Second Component
+		second_component = connected_components[1]
+
+		#Removal of Nodes - For First Component
+		for item in first_graph.nodes():
+			if item not in first_component:
+				first_graph.remove_node(item)
+
+		#Removal of Edges - For First Component
+		for item in first_graph.edges():
+			#First Node
+			source = item[0]
+			#Second Node
+			target = item[1]
+			#Nodes corresponding to the current edge does not belong to the first component
+			if source not in first_component and target not in first_component:
+				first_graph.remove_edge(source,target)
+
+
+		#Removal of Nodes - For Second Component
+		for item in second_graph.nodes():
+			if item not in second_component:
+				second_graph.remove_node(item)
+
+
+		#Removal of Edges - For Second Component
+		for item in second_graph.edges():
+			#First Node
+			source = item[0]
+			#Second Node
+			target = item[1]
+			#Nodes corresponding to the current edge does not belong to the second component
+			if source not in second_component and target not in second_component:
+				second_graph.remove_edge(source,target)
+
+
+
+		""" Testing Criterion : total = len(cut_edges) + len(first_graph.edges()) + len(second_graph.edges()) """
+
+		#if total != (len(cut_edges) + len(first_graph.edges()) + len(second_graph.edges())):
+		#	print "Error"
+
+        #print len(list(nx.connected_components(second_graph)))
+		#print len(list(nx.connected_components(second_graph)))
+
+
+		HCS(first_graph)
+		HCS(second_graph)
+
+
+
+		
+
+
+
+
+
+
 
  
 
@@ -83,6 +189,17 @@ def main_clustering(component, main_graph):
 	#Forming a temporary Graph Structure for the connected component
 	temp_graph = nx.Graph()
 	temp_graph = main_graph.copy()
+
+
+	""" According to Network Graph Data-structure -- it is essential to remove both nodes and edges to modify a graph """
+
+	#Removal of the nodes from temp_graph which are present not in component
+	for item in temp_graph.nodes():
+		if item not in component:
+			temp_graph.remove_node(item)
+
+	
+	#Removal of edges from the temp_graph which are not present in component
 	for item in temp_graph.edges():
 		#First Node
 		source = item[0]
@@ -93,11 +210,38 @@ def main_clustering(component, main_graph):
 			temp_graph.remove_edge(source,target)
 
 
-	#End of removal of redundant edges
-	#print len(temp_graph.edges())
 
-	#Find the edges involved in minimum cut
+	#End of removal of redundant edges
+	
+
+	#Find the edges involved in minimum cut -- "They have to be removed to obtain two components"
 	cut_edges = find_minimum_cut(temp_graph)
+
+	
+
+	""" Tests : Number of Connected Components == 1 """
+	#print len(list(nx.connected_components(temp_graph)))	
+
+	""" At this stage we have a connected component & a list of cut-edges on whose removal the graph will become disconnected """
+
+	#print cut_edges
+
+	""" Tests : Number of Connected Components == 2"""
+	#for item in cut_edges:
+	#	temp_graph.remove_edge(item[0],item[1])
+
+	#print len(list(nx.connected_components(temp_graph)))
+
+	
+	#Calling HCS function : @parameters => #temp_graph : Connected Graph
+	HCS(temp_graph)
+
+
+
+
+
+
+
 
 
 	
@@ -167,6 +311,8 @@ for component in g:
 	#Calls main_clustering each time with connected components : list(component) - (List of nodes in that component) & Original Graph Structure 
 	main_clustering(list(component),graph)
 	#print len(list(component))
+	break
+	
 
 
 
