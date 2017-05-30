4a187e0b6425d63cb34ac13595e53660bedd13d1
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-05-30 21:58:18 +0530

    Small Change

--- spectral.py
+++ spectral.py
@@ -16,5 +16,38 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 import sys
 import numpy as np 
+import collections
+
+#Base function for making function calls
+def main_operation():
+	#Loading gene interactions JSON file into a variable 
+	with open('JSON rows/gene_interactions.json') as json_data:
+		interactions = json.load(json_data)
 
 
+	#Information about Graph Connectivity
+	graph_info = interactions["results"]
+
+	#Creating NetworkX instance
+	graph = nx.Graph()
+
+	i = 0
+	#Extracting the edge relationships
+	for edge in graph_info:
+		#Adding the edge in NetworkX
+		graph.add_edge(edge[0],edge[2])
+
+	#Converting into numpy array
+	adjacency_matrix = nx.to_numpy_matrix(graph)
+
+	
+
+
+
+
+
+	
+
+
+
+main_operation()
