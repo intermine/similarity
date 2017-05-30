1c5267557b104b0f5654664409ce165aa043a560
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-05-30 18:48:51 +0530

    Added basic elements in the script

---
+++ spectral.py
@@ -0,0 +1,20 @@
+
+"""  InterMine @ Open Genome Informatics : Similarity Project
+   -> Application of the Spectral Clustering Methods to understand similar nodes in the Graph
+   -> Treatment of the Data Set as Undirected Graph -- Just Interactions taken into account """
+
+
+#Libraries 
+import json
+import pandas as pd
+from py2neo import Node, Relationship, Graph
+import re
+import string
+import networkx as nx
+from networkx.algorithms.connectivity import minimum_st_edge_cut,minimum_edge_cut
+import matplotlib.pyplot as plt
+from matplotlib import pylab
+import sys
+import numpy as np 
+
+
