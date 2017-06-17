c513b518208195e33f9b1123663a41c5b0eb0bb3
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-06-17 18:26:41 +0530

    Completed: Clustering Genes based on their characteristic information and treating each as a document

--- doc_cluster.py
+++ doc_cluster.py
@@ -21,9 +21,10 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 import sys
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans,AgglomerativeClustering 
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.cluster import KMeans,AgglomerativeClustering, MiniBatchKMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn.preprocessing import normalize
 from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 from categorical_cluster import hierarchical_mixed
@@ -32,6 +33,32 @@ from sklearn import (manifold, datasets, decomposition, ensemble,
              discriminant_analysis, random_projection)
 
 from features import create_features, get_genes
+
+#Function to compute the Clusters corresponding the Genetic Sets
+def compute_clusters(features):
+	#Conversion into numpy array
+	features =  np.array(features)
+
+	#As the matrix is sparse with lots of zeros - SVD works better than PCA as it does not centre the data
+	svd = TruncatedSVD(n_components=100,random_state=42)
+
+	#Obtaining Features after dimensionality reduction
+	transformed_features = svd.fit_transform(features)
+
+	#Normalize the Transformed Feature into Unit Norm so that K-means behave as spherical K-means
+	normalized_features = normalize(transformed_features)
+
+	#Applying K-means Algorithm - Initialization
+	k_means = KMeans(n_clusters=2, init='k-means++', n_init=10)
+
+	#Fitting the Feature Set into Mini Batch K-means
+	k_means.fit(normalized_features)
+
+	#Cluster Index
+	final_index = k_means.labels_
+
+	return final_index
+
 
 #Term frequency for a Genetic Term : Log Normalization
 def compute_term_frequency(document,word):
@@ -113,9 +140,13 @@ def main_operation():
 	#Computing singular sets for each gene as a document =: Type : Lists
 	gene_documents = create_gene_documents(feature_array)
 
+	#Conversion into Feature Vectors
 	tfidf_vectors = compute_tfidf(gene_documents)
 
-	similar_sets = compute_clusters(tfidf_vectors)
+	#Obtaining the cluster labels
+	cluster_labels = compute_clusters(tfidf_vectors)
+
+
 
 
 
