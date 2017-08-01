3c0adc302db13e4233ff41072bd442fb177dcc43
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2017-08-01 11:25:49 +0200

    Correction to extraction of top eigen values

--- spectral.py
+++ spectral.py
@@ -1,7 +1,7 @@
 
 """  InterMine @ Open Genome Informatics : Similarity Project
    -> Application of the Spectral Clustering Methods to understand similar nodes in the Graph
-   -> Treatment of the Data Set as Undirected Graph -- Just Interactions taken into account """
+   -> Treatment of the Data Set as Undirected Graph -- Gene Interactions taken into account """
 
 
 #Libraries 
@@ -22,37 +22,44 @@ import scipy.sparse as sparse
 
 '''
    Spectral Clustering Algorithm : 
-      -> Based on the connectivity between the nodes
-      Steps: 
-            1. Compute Adjacency Matrix for the given Graph
-            2. Compute the Laplacian Matrix for the Graph (Diagonal Degree Matrix - Adjacency Matrix)
-            3. Normalize the matrix
-            4. Get the Eigen Values and Eigen Vectors corresponding to the Normalized Laplacian Matrix
-            5. Choose the number of clusters (say k), then choose k eigen vectors corresponding to eigen values
-            6. Apply K-means on them to cluster the nodes with number of clusters as k
+	  -> Based on the connectivity between the nodes
+	  Steps: 
+			1. Compute Adjacency Matrix for the given Graph
+			2. Compute the Laplacian Matrix for the Graph (Diagonal Degree Matrix - Adjacency Matrix)
+			3. Normalize the matrix
+			4. Get the Eigen Values and Eigen Vectors corresponding to the Normalized Laplacian Matrix
+			5. Choose the number of clusters (say k), then choose k eigen vectors corresponding to eigen values
+			6. Apply K-means on them to cluster the nodes with number of clusters as k
 
-      @_parameters : 
-         lap => normalized laplacian matrix
-         eigen_number => The number of Eigen vectors to be obtained
+	  @_parameters : 
+		 lap => normalized laplacian matrix
+		 eigen_number => The number of Eigen vectors to be obtained
 
-      @_return :
-         A numpy array with cluster labels corresponding to each node 
+	  @_return :
+		 A numpy array with cluster labels corresponding to each node 
 '''
 
 #Function to compute the eigen vectors and eigen value & Apply K-means
 def spectral_clustering(lap,eigen_number):
 
-	#Eigen Values and Eigen Values for the Normalized Laplacian Sparse Array
-	w, v = sparse.linalg.eigs(lap,k=eigen_number)	
+	#Eigen Values and Eigen Vectors for the Normalized Laplacian Matrix
+	w,v = np.linalg.eigh(lap)
+
+	#Convert the Eigen Vector to list 
+	eigen_vectors = v.tolist()
+
+	#Extraction of the indexes of the top few eigen values
+	indexes = sorted(range(len(w)), key=lambda j: w[j])[-10:]
+	
 	
 	#Apply K-means to cluster
-	clustered = KMeans(n_clusters=eigen_number,random_state=10)
+	#clustered = KMeans(n_clusters=eigen_number,random_state=10)
 
 	#Predict the Labels
-	cluster_labels = clustered.fit_predict(v)
+	#cluster_labels = clustered.fit_predict(v)
 
 	#Returns a numpy array consisting of cluster assignment to each node
-	return cluster_labels
+	return #cluster_labels
 
 
 
@@ -76,20 +83,39 @@ def main_operation():
 		#Adding the edge in NetworkX
 		graph.add_edge(edge[0],edge[2])
 
+		if i == 200:
+			break
+
+		i += 1
+
 	#Converting into numpy array
 	adjacency_matrix = nx.to_numpy_matrix(graph)
 
+	#Checking if the Adjacency Matrix Generated is symmetric
+	#print np.allclose(adjacency_matrix,adjacency_matrix.T,atol=1e-8)
+
 	#Normalized Laplacian Matrix
 	normalized_laplacian = nx.normalized_laplacian_matrix(graph)
+
+	#Normal Laplacian Matrix
+	laplacian = nx.laplacian_matrix(graph)
+    
+    #Conversion to dense matrix
+	dense_laplacian = laplacian.todense()
+
+	#Conversion into Dense Matrix
+	normalized_laplacian_dense = normalized_laplacian.todense()
 
 	#The number of eigen vectors to be obtained for K-means clustering
 	eigen_number = 2
 
 	#Function Call for Spectral Clustering
-	cluster_labels = spectral_clustering(normalized_laplacian,eigen_number)
+	cluster_labels = spectral_clustering(normalized_laplacian_dense,eigen_number)
 
 	#List of Nodes
 	nodes = graph.nodes()
+
+	print len(nodes)
 
 	#Cluster consisting of similar nodes
 	clusters = {}
@@ -117,8 +143,8 @@ def main_operation():
 
 	#Drawing the Graph
 	values = [cluster_dict.get(node,0.25) for node in graph.nodes()]
-	nx.draw(graph,cmap=plt.get_cmap('jet'),node_color = values)
-	plt.show()
+	#nx.draw(graph,cmap=plt.get_cmap('jet'),node_color = values)
+	#plt.show()
 
 
 
