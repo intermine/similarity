cc7306f49cdcd8570262351dad23db955c530684
aswanipranjal <aswani.pranjal@gmail.com>
2018-01-06 18:04:34 +0530

    The graph is now generated in a bounded svg container. This allows us to see all the nodes.

--- visualisations.py
+++ visualisations.py
@@ -15,28 +15,53 @@ gene_data = [literal_eval(line.strip()) for line in file if line.strip()]
 
 G = nx.Graph() # Initialize the graph
 
+"""
+# Experimental Code
+# -----------------------------------------------------------------------------------------------------------------
+# This function is used to normalize the data.
+def normalize(data_list):
+	min_val = min(data_list)
+	max_val = max(data_list)
+	return [ float(item - min_val) / float(max_val - min_val) for item in data_list ]
+
+# The available weights are normalized so that they can be used to show the strength of the link between two nodes
+weights = normalize([item[1] for item in gene_data])
+
+print(weights)
+
+G = nx.Graph() # Initialize the graph
+
+# Adding all the nodes in the Graph and creating edges
+for i in range(len(gene_data)):
+	G.add_edge(gene_data[i][0], gene_da ta[i][1], weight=weights[i]) # The data is in the form: ((gene1, gene2), \
+	weight)
+
+print(G.edges())
+"""
+
 # Adding all the nodes in the Graph and creating edges
 for item in gene_data:
 	G.add_edge(item[0][0], item[0][1], weight=item[1]) # The data is in the form: ((gene1, gene2), weight)
 
+print(G.edges)
 def get_graph_features(graph):
 	# Finding the degree and parity of each node. The degree will help us color the nodes when using D3.js
-	print "Calculating the degree and parity for each node"
+	print "Calculating the degree and parity for each node..."
 	for ix, deg in dict(graph.degree()).items():
 		graph.node[ix]['degree'] = deg
-		graph.node[ix]['parity'] = (1-deg%2)
+		#graph.node[ix]['parity'] = (1-deg%2)
 	
 	# Finding Katz centrality of each node. We can use this to decide the width of the links between nodes in the graph 
-	print "Generating Katz centrality for each node"
+	print "Generating Katz centrality for each node..."
 	for ix, katz in nx.katz_centrality(graph, max_iter=max_iterations).items():
 		graph.node[ix]['katz'] = katz
-
+	
 	return graph
 
 G = get_graph_features(G)
 data = json_graph.node_link_data(G)
 
-print "Writing to 'Visualisations/top_edges_grap.json' file"
+print "Writing to 'Visualisations/top_edges_grap.json' file..."
 with open('Visualisations/top_edges_graph.json', 'w') as file:
 	json.dump(data, file, indent=4)
 
